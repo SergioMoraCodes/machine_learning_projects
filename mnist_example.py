a49ef3e73b68fe688b73e00791d7d18a871eb667
Sergio Mora <seres.mora@hotmail.com>
2022-03-20 18:53:28 -0500

    sequential (layers) approach to model the network, basic structure with keras.layers

--- mnist_example.py
+++ mnist_example.py
@@ -2,6 +2,35 @@ import tensorflow as tf
 from keras.datasets import mnist
 import matplotlib.pyplot as plt
 import numpy as np
+from tensorflow.keras.layers import Conv2D, Input, Dense, MaxPool2D, BatchNormalization, Flatten, GlobalAvgPool2D
+
+
+
+# tensorflow.keras.Sequential
+
+model= tf.keras.Sequential(
+    [
+        Input(shape=(28,28,1)), #size of the image, 1 represent the color chanel
+        Conv2D(32, (3,3), activation='relu'), #[0] how many filters are [1] size of the filter
+                                             #[2] 'relu'= rectified linear unit
+        Conv2D(64, (3,3), activation='relu'),
+        MaxPool2D(),
+        BatchNormalization(),
+
+        Conv2D(128, (3,3), activation='relu'),
+        MaxPool2D(),
+        BatchNormalization(),
+
+        GlobalAvgPool2D(), #computes average of the output values according to some axes
+        Dense(64, activation='relu'), #vector containing values
+        Dense(10, activation='softmax') #returns a set of 10 possible values, in propabilities
+        
+    ]
+)
+
+#functional approach : function that returns a model
+
+# tensorflow.keras.Model : inherit from this class and modify the object
 
 def display_examples(examples, labels):
     plt.figure(figsize=(8,8))
@@ -17,7 +46,7 @@ def display_examples(examples, labels):
     plt.show()
 if __name__=='__main__':
     
-
+    #Dataset visualization
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
     print('x_train.shape = ', x_train.shape)
     print('y_train.shape = ', y_train.shape)
