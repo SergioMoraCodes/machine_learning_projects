cc7ba39e1ddcc94a691e9e90c71ab7362e15e2c2
Sergio Mora <seres.mora@hotmail.com>
2022-03-22 16:26:13 -0500

    finished the signs recognition project, creating a model to evaluate images above 96% accuracy, upgraded the utils and deeplearning_models files to future use

--- signs_recognition/signs_recognition.py
+++ signs_recognition/signs_recognition.py
@@ -1,18 +1,66 @@
-import os
-import glob
-from sklearn.model_selection import train_test_split
 
-def split_data(path_data, train_folder, val_folder, split_size=0.15):
+import tensorflow as tf
+from gc import callbacks
+from my_utils import split_data, order_test_set,create_generators
+from deeplearning_models import street_signs
+from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping
 
-    folders = os.listdir(path_data) #gives a list of all the folders in the directory
+if __name__=='__main__':
 
-    for folder in folders:
-        full_path = os.path.join(path_data,folder)
-        images_paths = glob.glob(os.path.join(full_path, '*.png'))
+    if False:
+        path = 'C:\\Users\\sergi\Documents\\PROGRAMAS APRENDIZAJE\\tensorflow\\signs_recognition\\Train'
+        path_train = 'C:\\Users\\sergi\\Documents\\PROGRAMAS APRENDIZAJE\\tensorflow\\signs_recognition\\_training\\train'
+        path_val = 'C:\\Users\\sergi\\Documents\\PROGRAMAS APRENDIZAJE\\tensorflow\\signs_recognition\\_training\\val'
 
-        x_train, x_val = train_test_split(images_paths, test_size=split_size)
+        split_data(path, path_train, path_val)
 
-        for x in x_train:
+        path_to_images = 'C:\\Users\\sergi\\Documents\\PROGRAMAS APRENDIZAJE\\tensorflow\\signs_recognition\\Test'
+        path_csv = 'C:\\Users\\sergi\\Documents\\PROGRAMAS APRENDIZAJE\\tensorflow\signs_recognition\\Test.csv'
+        order_test_set(path_to_images, csv_path=path_csv)
 
-            basename = os.path.basename(x)
-            path_to_folder = os.path.join()
+    path_train = 'C:\\Users\\sergi\\Documents\\PROGRAMAS APRENDIZAJE\\tensorflow\\signs_recognition\\_training\\train'
+    path_val = 'C:\\Users\\sergi\\Documents\\PROGRAMAS APRENDIZAJE\\tensorflow\\signs_recognition\\_training\\val'
+    path_test = 'C:\\Users\\sergi\\Documents\\PROGRAMAS APRENDIZAJE\\tensorflow\\signs_recognition\\Test'
+    batch_size = 64
+    epochs = 15
+
+    train_generator, val_generator, test_generator = create_generators(batch_size, path_train, path_val, path_test)
+    nbr_classes = train_generator.num_classes
+    TRAIN = False
+    TEST = True
+    if TRAIN:
+        path_to_save_model = './models'
+        check_saver = ModelCheckpoint(
+            path_to_save_model,
+            monitor='val_accuracy',
+            mode='max',
+            save_best_only=True,
+            save_freq='epoch',
+            verbose=1
+        )
+
+        check_stop = EarlyStopping(
+            monitor='val_accuracy',
+            patience=10
+        )
+
+
+        model = street_signs(nbr_classes)
+        model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+        model.fit(train_generator,
+                epochs=epochs,
+                batch_size=batch_size,
+                validation_data=val_generator,
+                callbacks=[check_saver,check_stop]
+
+        )
+
+    if TEST:
+        model = tf.keras.models.load_model('./models')
+        model.summary()
+
+        print('Evaluating Validation Set : ')
+        model.evaluate(val_generator)
+
+        print('Evaluating Test Set : ')
+        model.evaluate(test_generator)
