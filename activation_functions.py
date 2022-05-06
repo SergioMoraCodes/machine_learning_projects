5bfc6237df1f80faa301616b6281f28fd6c9bb3f
Sergio Mora <seres.mora@hotmail.com>
2022-05-05 21:58:19 -0500

    neural network from zero with numpy

--- activation_functions.py
+++ activation_functions.py
@@ -1,7 +1,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def sigmoid(x):
+def sigmoid(x, derivate=False):
+    if derivate:
+        return np.exp(-x)/((1+np.exp(-x))**2)
     return 1 / (1 + np.exp(-x))
 
 def threshold(a):
@@ -10,7 +12,11 @@ def threshold(a):
 def signum(a):
     return np.piecewise(a, [a<0, a>=0],[-1,1])
 
-def relu(a):
+def relu(x, derivate=False):
+    if derivate:
+        x[x <= 0] = 0
+        x[x >  0] = 1
+        return x
     #np.piecewise(a,[a<0, a>=0], [0,lambda a: a])
     return np.maximum(0,a)
 
